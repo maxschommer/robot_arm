e9e08066b3a8f526be4a33c4c9049d1463ceccf8
maxschommer <maxschommer@gmail.com>
2017-01-18 00:37:57 -0500

    Made a robot arm class, and changed line color to be one and not rainbow

--- arm_sim.py
+++ arm_sim.py
@@ -8,6 +8,28 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 plt.subplots_adjust(left=0.25, bottom=0.25)
 
+class Arm:
+	"""This holds all of the positions of all of the joints, and the axis of roation of all of the segments""" 
+
+	def __init__(self, rot_axis=[0,0,1], position=[0,0,0]):
+		self.rot_axis = [rot_axis]
+		self.position = [position]
+
+	def add_joint(self, rot_axis, position):
+		self.rot_axis.insert(0, rot_axis)
+		self.position.insert(0, position)
+	def plot_arm(self, color='b'):
+		self.fig = plt.figure()
+		self.ax = fig.add_subplot(111, projection="3d")
+	def __str__(self):
+		pos_str = str(self.position)
+		rot_str = str(self.rot_axis)
+		output = 'Position Array: ' + pos_str +'\n'+'Rotation Axis Array: ' + rot_str
+		return output
+
+arm = Arm()
+arm.add_joint([0,1,0], [None, None, None])
+print(arm)
 #Base Coordinate
 base = [0,0,0]
 should = [2,0,6]
@@ -52,8 +74,7 @@ def update(val):
     should_len = np.dot(rotation_matrix(axis_should,theta), should)
     temp = np.vstack((base, should_len))
     ax.lines.remove(prev_line)
-    prev_line, = ax.plot(*temp.T)
-
+    prev_line, = ax.plot(*temp.T, color='b')
     fig.canvas.draw_idle()
 
 srot_base.on_changed(update)
@@ -66,15 +87,5 @@ button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 def reset(event):
     srot_base.reset()
 button.on_clicked(reset)
-
-rax = plt.axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
-radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-
-
-def colorfunc(label):
-    b.set_color(label)
-    fig.canvas.draw_idle()
-
-radio.on_clicked(colorfunc)
 
 plt.show()
