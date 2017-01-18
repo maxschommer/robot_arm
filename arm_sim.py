fb676b692e19cc18494608ec32cc90b9aa2e1ec6
maxschommer <maxschommer@gmail.com>
2017-01-17 22:31:25 -0500

    Slider now rotates 3D line

--- arm_sim.py
+++ arm_sim.py
@@ -2,38 +2,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.widgets import Slider, Button, RadioButtons
+import math
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 plt.subplots_adjust(left=0.25, bottom=0.25)
-t = np.arange(0.0, 1.0, 0.001)
-### create constant z-coordinate
-z = np.zeros_like(t)    #               <------------ here
-a0 = 5
-f0 = 3
-s = a0*np.sin(2*np.pi*f0*t)
-l, = plt.plot(t, s, lw=2, color='red')
-plt.axis([0, 1, -10, 10])
 
-axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg="w")
-axamp = plt.axes([0.25, 0.15, 0.65, 0.03], axisbg="w")
+#Base Coordinate
+base = [0,0,0]
+should = [2,0,6]
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+should_len = np.vstack((base, should))
+b, = ax.plot(*should_len.T)
+prev_line = b
+
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis/math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta/2.0)
+    b, c, d = -axis*math.sin(theta/2.0)
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
+                     [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
+                     [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+
+
+ax.set_xlim3d(-10, 10)
+ax.set_ylim3d(-10, 10)
+ax.set_zlim3d(0, 10)
+
+axrot_base = plt.axes([.25, .2, 0.65, 0.03], axisbg="w")
+
+srot_base = Slider(axrot_base, "Base Rotation", 0, 2*3.14159, valinit=3.14159)
 
 
 def update(val):
-    amp = samp.val
-    freq = sfreq.val
-    #set constant z coordinate
-    l.set_data(t, z)  #               <------------ here
-    # set values to y-coordinate
-    l.set_3d_properties(amp*np.sin(2*np.pi*freq*t), zdir="y") #<------------ here
+    global prev_line
+    
+    theta = srot_base.val
+
+
+    axis_should = [0,0,1]
+    should_len = np.dot(rotation_matrix(axis_should,theta), should)
+    temp = np.vstack((base, should_len))
+    ax.lines.remove(prev_line)
+    prev_line, = ax.plot(*temp.T)
+
     fig.canvas.draw_idle()
 
-sfreq.on_changed(update)
-samp.on_changed(update)
-
+srot_base.on_changed(update)
 
 axcolor = 'lightgoldenrodyellow'
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
@@ -41,8 +64,7 @@ button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 
 
 def reset(event):
-    sfreq.reset()
-    samp.reset()
+    srot_base.reset()
 button.on_clicked(reset)
 
 rax = plt.axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
@@ -50,8 +72,9 @@ radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
 
 
 def colorfunc(label):
-    l.set_color(label)
+    b.set_color(label)
     fig.canvas.draw_idle()
+
 radio.on_clicked(colorfunc)
 
 plt.show()
